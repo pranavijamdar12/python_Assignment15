004a09c0e5f0f45bcb68cecd61d91716c483025b
pranavi jamdar <pranavijamdar39@gmail.com>
2026-07-07 17:10:18 +0530

    Implement odd number filter in q3.py
    Added a function to filter odd numbers from a list.

---
+++ q3.py
@@ -0,0 +1,11 @@
+CheckOdd = lambda no:(no % 2 != 0)
+
+def main():
+    Data = [10,20,35,40,50,69,75]
+    print("Input Data is:",Data)
+    FData = list(filter(CheckOdd,Data))
+    print("The filter Data is:",FData)
+    
+if __name__ == "__main__":
+    main()
+
